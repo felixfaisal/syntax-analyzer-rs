67456471ef64c64345e512971341ba4837a1406f
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-12-06 06:02:16 +0530

    Using command line argument

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod parser;
 mod tokenize;
 use tokenize::tokenize;
+use std::env;
 fn main() {
     //println!("Hello, world!");
-    let file = std::fs::read_to_string("test.anon").expect("Something went wrong");
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {panic!("Please provide the file path, Example: cargo run test.anon");}
+    let file = std::fs::read_to_string(&args[1]).expect("Something went wrong");
     tokenize(file);
 }
 
